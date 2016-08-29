201605a94d09478c1256103de9c98062c3854dc1
Justin Anderson <janderson2k@gmail.com>
2016-08-28 20:53:49 -0700

    added tests to rulers gem

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,10 +1,12 @@
 require "rulers/version"
+require 'rulers/array'
 
 module Rulers
   class Application
     def call(env)
+      'echo debug > debug.txt';
       [200, {'Content-Type' => 'text/html'},
-        ["Hello form Ruby on Rulers!"]]
+        ["Hello from Ruby on Rulers!"]]
+      end
     end
-  end
 end
